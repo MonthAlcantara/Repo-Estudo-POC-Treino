0e52b0bdce20e56e83d888cbb848cae320cab7ec
monthalcantara <montival_junior@yahoo.com.br>
2021-03-29 15:47:15 -0300

    Adicionado projeto biblioteca seguindo CDD

---
+++ API-Biblioteca-DevEficiente-CDD/src/main/java/io/github/monthalcantara/bibliotecaCDD/BibliotecaCddApplication.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.bibliotecaCDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaCddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaCddApplication.class, args);
+	}
+
+}
---
+++ API-Biblioteca-DevEficiente-CDD/src/test/java/io/github/monthalcantara/bibliotecaCDD/BibliotecaCddApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.bibliotecaCDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaCddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
