a81a74b1c78be9a9bc8c578b067d19ec9e39824a
monthalcantara <montival_junior@yahoo.com.br>
2021-05-11 22:49:19 -0300

    Adicionado novo projeto

---
+++ API-Banco-Digital-Zup/src/main/java/io/github/monthalcantara/bancodigital/BancodigitalApplication.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.bancodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancodigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancodigitalApplication.class, args);
+	}
+
+}
---
+++ API-Banco-Digital-Zup/src/test/java/io/github/monthalcantara/bancodigital/BancodigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.bancodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancodigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
