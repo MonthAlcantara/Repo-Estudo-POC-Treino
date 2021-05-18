67a6182cbf9741d3e1af22ce64efd2b2bc43caf3
monthalcantara <montival_junior@yahoo.com.br>
2021-05-18 12:46:16 -0300

    [wip] ajustes anotacoes

--- API-Banco-Digital-Zup/io.github.monthalcantara.repository/src/main/java/io/github/monthalcantara/repository/adapter/ClientePersistence.java
+++ API-Banco-Digital-Zup/io.github.monthalcantara.repository/src/main/java/io/github/monthalcantara/repository/adapter/ClientePersistence.java
@@ -1,9 +1,11 @@
 package io.github.monthalcantara.repository.adapter;
 
 import io.github.monthalcantara.repository.model.ClienteEntity;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@EnableScan
 public interface ClientePersistence extends CrudRepository<ClienteEntity, String> {
 }
--- API-Banco-Digital-Zup/io.github.monthalcantara.repository/src/main/java/io/github/monthalcantara/repository/config/DynamoDBConfiguration.java
+++ API-Banco-Digital-Zup/io.github.monthalcantara.repository/src/main/java/io/github/monthalcantara/repository/config/DynamoDBConfiguration.java
@@ -1,18 +1,19 @@
 package io.github.monthalcantara.repository.config;
 
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 @Configuration
+@EnableDynamoDBRepositories
+        (basePackages = "io.github.monthalcantara.repository.adapter")
 public class DynamoDBConfiguration {
 
     @Value("${amazon.dynamodb.endpoint}")
@@ -28,21 +29,20 @@ public class DynamoDBConfiguration {
     private String amazonAwsRegion;
 
     @Bean
-    public DynamoDBMapperConfig dynamoDBMapperConfig() {
-        return DynamoDBMapperConfig.DEFAULT;
-    }
-
-    @Bean
-    public DynamoDBMapper dynamoDBMapper(final AmazonDynamoDB dynamoDBMapper) {
-        return new DynamoDBMapper(dynamoDBMapper);
-    }
-
-    @Bean
     public AmazonDynamoDB amazonDynamoDB() {
-        return AmazonDynamoDBClientBuilder
-                .standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(amazonDynamoDBEndpoint, amazonAwsRegion))
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey)))
-                .build();
+        AmazonDynamoDB amazonDynamoDB
+                = new AmazonDynamoDBClient(amazonAWSCredentials());
+
+        if (!StringUtils.isEmpty(amazonDynamoDBEndpoint)) {
+            amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
+        }
+
+        return amazonDynamoDB;
+    }
+
+    @Bean
+    public AWSCredentials amazonAWSCredentials() {
+        return new BasicAWSCredentials(
+                amazonAWSAccessKey, amazonAWSSecretKey);
     }
 }
