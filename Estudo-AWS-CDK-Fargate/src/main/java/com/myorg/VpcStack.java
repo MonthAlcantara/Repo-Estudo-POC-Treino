63cc7086e83abe1c6edf913e7b91c3c5f031d990
monthalcantara <montival_junior@yahoo.com.br>
2021-05-19 16:32:09 -0300

    [wip] Adicionada stack vpc

--- Estudo-AWS-CDK-Fargate/src/main/java/com/myorg/VpcStack.java
+++ Estudo-AWS-CDK-Fargate/src/main/java/com/myorg/VpcStack.java
@@ -15,7 +15,7 @@ public class VpcStack extends Stack {
 
         /*
          * Para criar uma vpc eu preciso de um escopo, que seria a classe onde eu estou 'this'
-         * e mais um nome para identificar o servico
+         * e mais um nome para identificar o servico.
          */
        Vpc.Builder.create(this, "Vpc01")
                //Quantidade maximas de zonas de disponibilidades que minha vpc atuará
