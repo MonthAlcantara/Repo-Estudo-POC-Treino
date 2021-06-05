3133cc2479496bacafe7af9e26b92c813c1d8620
monthalcantara <montival_junior@yahoo.com.br>
2021-06-05 12:28:02 -0300

    [wip] config imports modules

--- API-Banco-Digital-Zup/io.github.monthalcantara.repository/src/main/java/io/github/monthalcantara/repository/adapter/ClientePersistence.java
+++ API-Banco-Digital-Zup/io.github.monthalcantara.repository/src/main/java/io/github/monthalcantara/repository/adapter/ClientePersistence.java
@@ -1,12 +1,7 @@
 package io.github.monthalcantara.repository.adapter;
 
-import io.github.monthalcantara.core.persistence.ClientePersistence;
-import io.github.monthalcantara.repository.model.ClienteEntity;
-import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@EnableScan
-interface ClientePersistenceImpl extends CrudRepository<ClienteEntity, String>, ClientePersistence {
+interface ClientePersistenceImpl {
 }
--- API-Banco-Digital-Zup/io.github.monthalcantara.repository/src/main/java/io/github/monthalcantara/repository/model/ClienteEntity.java
+++ API-Banco-Digital-Zup/io.github.monthalcantara.repository/src/main/java/io/github/monthalcantara/repository/model/ClienteEntity.java
@@ -1,9 +1,5 @@
 package io.github.monthalcantara.repository.model;
 
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBIndexHashKey;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,25 +7,25 @@ import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
-@DynamoDBTable(tableName = "Cliente")
+//@DynamoDBTable(tableName = "Cliente")
 public class ClienteEntity {
 
-    @DynamoDBHashKey(attributeName = "cpf")
+//    @DynamoDBHashKey(attributeName = "cpf")
     private String cpf;
 
-    @DynamoDBIndexHashKey(attributeName = "email")
+//    @DynamoDBIndexHashKey(attributeName = "email")
     private String email;
 
-    @DynamoDBAttribute(attributeName = "nome")
+//    @DynamoDBAttribute(attributeName = "nome")
     private String nome;
 
-    @DynamoDBAttribute(attributeName = "sobrenome")
+//    @DynamoDBAttribute(attributeName = "sobrenome")
     private String sobrenome;
 
-    @DynamoDBAttribute(attributeName = "data_de_nascimento")
+//    @DynamoDBAttribute(attributeName = "data_de_nascimento")
     private LocalDate dataNascimento;
 
-    @DynamoDBAttribute(attributeName = "data_de_criacao")
+//    @DynamoDBAttribute(attributeName = "data_de_criacao")
     private LocalDate dataCriacao;
 
 }
