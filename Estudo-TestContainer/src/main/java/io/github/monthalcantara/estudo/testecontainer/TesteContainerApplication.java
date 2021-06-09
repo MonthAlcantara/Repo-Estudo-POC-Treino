c661bcb7ad5499f8edf1b35283e3cd35bd2a93fb
monthalcantara <montival_junior@yahoo.com.br>
2021-06-09 20:25:15 -0300

    Adicionado novo repo de estudo sobre testContainer

---
+++ Estudo-TestContainer/src/main/java/io/github/monthalcantara/estudo/testecontainer/TesteContainerApplication.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.estudo.testecontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteContainerApplication.class, args);
+	}
+
+}
---
+++ Estudo-TestContainer/src/test/java/io/github/monthalcantara/estudo/testecontainer/TesteContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.estudo.testecontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
