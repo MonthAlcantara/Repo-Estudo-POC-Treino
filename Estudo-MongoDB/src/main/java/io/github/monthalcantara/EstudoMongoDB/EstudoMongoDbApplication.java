2ccf9b179335c37987d21510ba7101be862c875a
monthalcantara <montival_junior@yahoo.com.br>
2021-08-01 19:38:39 -0300

    Adicionado projeto de estudo mongoDB

---
+++ Estudo-MongoDB/src/main/java/io/github/monthalcantara/EstudoMongoDB/EstudoMongoDbApplication.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.EstudoMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoMongoDbApplication.class, args);
+	}
+
+}
---
+++ Estudo-MongoDB/src/test/java/io/github/monthalcantara/EstudoMongoDB/EstudoMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.EstudoMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
