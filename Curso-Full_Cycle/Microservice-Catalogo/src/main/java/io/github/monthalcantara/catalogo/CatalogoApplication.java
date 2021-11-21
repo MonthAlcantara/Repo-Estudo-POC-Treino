c0b9b9675188731e3cc31b8cd12f6dd0f91b10d1
MonthAlcantara <montival_junior@yahoo.com.br>
2021-11-21 17:23:09 -0300

    Adicionado novo projeto

---
+++ Curso-Full_Cycle/Microservice-Catalogo/src/main/java/io/github/monthalcantara/catalogo/CatalogoApplication.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.catalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoApplication.class, args);
+	}
+
+}
---
+++ Curso-Full_Cycle/Microservice-Catalogo/src/test/java/io/github/monthalcantara/catalogo/CatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
